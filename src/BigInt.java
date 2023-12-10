7bc9eb06faa611c0d74151880e05288593e44a19
Ashton <treixatek@gmail.com>
2023-12-10 14:44:37 -0500

    Take input & store in ArrayList

---
+++ src/BigInt.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class BigInt {
+    ArrayList number1;
+    ArrayList number2;
+    ArrayList answer;
+
+    //the constructor
+    public BigInt(ArrayList<Integer> list1, ArrayList<Integer> list2){
+        number1 = (ArrayList)list1.clone();
+        number2 = (ArrayList)list2.clone();
+    }
+
+    // clears the lists so you can try another example.
+    public void clearArraylist(){
+        while (!number1.isEmpty()){
+            number1.remove(0);
+        }
+        while (!number2.isEmpty()){
+            number2.remove(0);
+        }
+        while (!answer.isEmpty()){
+            answer.remove(0);
+        }
+    }
+
+    // calculates the sum of the two #’s.
+    public void addNums(){
+
+    }
+    // calculates the difference of the two #’s.
+    public void subNums(){
+
+    }
+
+    // returns the answer for printing.
+    public ArrayList getAnswer(){
+
+    }
+}
---
+++ src/BigIntDriver.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BigIntDriver {
+    public static void main(String[] args) {
+        ArrayList<Integer> num1 = new ArrayList<>();
+        ArrayList<Integer> num2 = new ArrayList<>();
+
+        String input;
+        Scanner fina = new Scanner(System.in);
+
+        //read input and store in arraylists
+        for (int j = 0; j < 2; j++) {
+            System.out.println("Enter a number");
+            input = fina.nextLine();
+
+            for (int i = 0; i < input.length(); i++) {
+                int digit = Integer.parseInt(input.substring(i, i+1));
+                if (j == 0)
+                    num1.add(digit);
+                else
+                    num2.add(digit);
+            }
+        }
+
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
